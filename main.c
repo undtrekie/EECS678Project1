c4b86305396457b8efdb37ca9e276b9a0e080de9
steinertyler@gmail.com <steinertyler@gmail.com>
2015-02-17 13:14:44 -0600

    Rest of commands in menu/help

--- main.c
+++ main.c
@@ -6,8 +6,12 @@
 int main(){
     char input[MAX_LENGTH];
     char exit[]="exit";
+    char quit[]="quit";
     char help[]="help";
-    while(strcmp(input, exit)!=0){
+    char set[]="set";
+    char cd[]="cd";
+    char jobs[]="jobs";
+    while(strcmp(input, exit)!=0 && strcmp(input, quit)!=0){
 	printf("quash# > ");
 	scanf("%s", input);
 
@@ -15,6 +19,18 @@ int main(){
 	    printf("commands:\n");
 	    printf("\thelp\n");
 	    printf("\texit\n");
+	    printf("\tquit\n");
+	    printf("\tset\n");
+	    printf("\tcd\n");
+	    printf("\tjobs\n");
+	}else if(strcmp(input, set)==0){
+	    
+	}else if(strcmp(input, cd)==0){
+
+	}else if(strcmp(input, jobs)==0){
+
+	}else{
+	    printf("%s: command not found.\n", input);
 	}
     }
     return 0;
