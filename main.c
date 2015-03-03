1a4109cfbfc5daa4fb48182a71670750907c1d7c
steinertyler@gmail.com <steinertyler@gmail.com>
2015-03-02 22:24:54 -0600

    Implemented 'cd' Functionality--done

--- main.c
+++ main.c
@@ -6,20 +6,38 @@
 */
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+
 
 #define MAX_LENGTH 80
 
-int main(){
+int main(int argc, char** argv){
     char input[MAX_LENGTH];
+    char* home=argv[1];
+    char wDirectory[MAX_LENGTH];
+    char* path=argv[2];
     char exit[]="exit";
     char quit[]="quit";
     char help[]="help";
     char set[]="set";
     char cd[]="cd";
     char jobs[]="jobs";
+    int num=0;
+    //Set Working Directory
+
+    if(argc!=3){
+	printf("\n%i: Incorrect # of parameters\n\n",argc);
+	printf("usage: ./project <home directory> <path>\n\n");
+	return 1;
+    }
+    strcpy(wDirectory, home);
+    
+
     while(strcmp(input, exit)!=0 && strcmp(input, quit)!=0){
-	printf("quash# > ");
-	scanf("%s", input);
+	printf("quash:%s > ", wDirectory);
+	//fgets(input, MAX_LENGTH, stdin);
+	//strtok(input, "\n");
+      	scanf("%s", input);
 
 	if(strcmp(input, help)==0){
 	    printf("commands:\n");
@@ -29,10 +47,21 @@ int main(){
 	    printf("\tset\n");
 	    printf("\tcd\n");
 	    printf("\tjobs\n");
-	}else if(strcmp(input, set)==0){
+	}else if(strncmp(input, set, 3)==0){
 	    
-	}else if(strcmp(input, cd)==0){
-
+	}else if(strncmp(input, cd, 2)==0){
+	    char buff[MAX_LENGTH];
+	    fgets(buff, 80, stdin);
+	    if(strlen(buff)>1){
+		strtok(buff, "\n");
+		int i;
+		for(i=0; i<strlen(buff)-1; i++){
+		    wDirectory[i]=buff[i+1];
+		}
+		wDirectory[i]='\0';
+	    }else{
+		strcpy(wDirectory, home);
+	    }
 	}else if(strcmp(input, jobs)==0){
 	
 	}else if(strcmp(input, quit)==0){
